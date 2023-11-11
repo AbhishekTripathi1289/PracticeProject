da9df6bafbf837dd3e8e7f2cc7b57fd8d2b4b94f
Abhishek Tripathi <tripathiabhishek1289@gmail.com>
2023-11-11 11:27:25 +0530

    practice

---
+++ app/src/main/java/com/example/myapplication/ui/theme/PracticeKotlin.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication.ui.theme
+
+import androidx.compose.ui.text.toUpperCase
+
+
+fun main(args: Array<String>) {
+   var b = mutableMapOf<String,String>("dfsa" to "fasdf","sdfa"  to "safd" ,"wer"  to "dsaf")
+    var a = mutableSetOf<String>("dfsa" )
+
+
+    var callback  = { a: Int, b: String -> String
+        "Abhishek Tripathi"
+    }
+   // var listsec = mutableListOf<String>("qaz", "dfd", "dsaf")
+    var listFirst = mutableListOf(mutableListOf<Person>(
+        Person(1, "www"),
+        Person(2, "zzz"),
+        Person(3, "rrr"),
+        Person(1, "eee"),
+        Person(4, "wwww"))
+    )
+
+
+}
+
+
+data class Person(var id: Int, var name: String)
+
